660c7fab3e3248f186c626816fb03562a669d3a4
David Gubala <gubaladev@gmail.com>
2021-06-26 15:53:24 -0500

    import graphql and sequelize

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize'; // importing orm to use with postgresql
+import { GraphQLModule } from '@nestjs/graphql'; //importing graphql
+import { join } from 'path';
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'todo-api',
+      models: [],
+    }),
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
